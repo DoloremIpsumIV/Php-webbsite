6c2bc452581bd96f4d6cf770fe663195d3a363d8
Rasmus Jildholt <Rjildholt@gmail.com>
2024-10-16 17:49:57 +0200

    whatevs done

--- users.php
+++ users.php
@@ -4,7 +4,6 @@ require_once "user.php";
 class Users
 {
     private $users = ["Tom" => "Password231", "Berry" => "Berry", "Garry" => "jkX24I!"];
-
     public function find(string $username, string $password)
     {
         if (array_key_exists($username, $this->users)) {
